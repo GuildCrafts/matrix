ffe4054d570343b1fa775e125003caba17b82230
Shereef Bishay <shereef@gmail.com>
2017-02-18 14:20:29 -0800

    adds js competency matrix

--- js/cards.js
+++ js/cards.js
@@ -1,3 +1,32 @@
+Handlebars.registerHelper('levelClass', function(level) {
+  switch(level) {
+    case 0:
+        return "card-inverse card-primary";
+        break;
+    case 1:
+        return "card-inverse card-primary";
+        break;
+    case 2:
+        return "card-inverse card-success";
+        break;
+    case 3:
+        return "card-inverse card-info";
+        break;
+    case 4:
+        return "card-inverse card-warning";
+        break;
+    case 5:
+        return "card-inverse card-danger";
+        break;
+    case 6:
+        return "card-inverse card-danger";
+        break;
+    default:
+        return "";
+      }
+});
+
+
 function loadCards() {
 
   var template = Handlebars.compile($('#row_template').html());
